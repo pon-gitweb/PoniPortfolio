c79166ba8077136f4656aec83f93200b6cab81b7
pon-gitweb <89674119+pon-gitweb@users.noreply.github.com>
2022-05-05 16:25:31 +1200

    Added data

---
+++ PoniPortfolio/Controllers/AdminSkillsController.cs
@@ -0,0 +1,154 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PoniPortfolio.Data;
+using PoniPortfolio.Models;
+
+namespace PoniPortfolio.Controllers
+{
+    public class AdminSkillsController : Controller
+    {
+        private readonly PoniPortfolioContext _context;
+
+        public AdminSkillsController(PoniPortfolioContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Skills
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Skills.ToListAsync());
+        }
+
+        // GET: Skills/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var skills = await _context.Skills
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (skills == null)
+            {
+                return NotFound();
+            }
+
+            return View(skills);
+        }
+
+        // GET: Skills/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Skills/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Percentage")] Skills skills)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(skills);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(skills);
+        }
+
+        // GET: Skills/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var skills = await _context.Skills.FindAsync(id);
+            if (skills == null)
+            {
+                return NotFound();
+            }
+            return View(skills);
+        }
+
+        // POST: Skills/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Percentage")] Skills skills)
+        {
+            if (id != skills.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(skills);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SkillsExists(skills.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(skills);
+        }
+
+        // GET: Skills/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var skills = await _context.Skills
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (skills == null)
+            {
+                return NotFound();
+            }
+
+            return View(skills);
+        }
+
+        // POST: Skills/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var skills = await _context.Skills.FindAsync(id);
+            _context.Skills.Remove(skills);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SkillsExists(int id)
+        {
+            return _context.Skills.Any(e => e.Id == id);
+        }
+    }
+}
